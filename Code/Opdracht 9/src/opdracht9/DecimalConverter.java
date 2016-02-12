0aba03e8d6dfd5aebf1becdb5727a670b90ce7db
Marijn Pool <info@marijnpool.nl>
2016-02-12 13:28:41 +0100

    Opdracht 9 en 10 toegevoegd

---
+++ Code/Opdracht 9/src/opdracht9/DecimalConverter.java
@@ -0,0 +1,11 @@
+package opdracht9;
+
+public class DecimalConverter {
+	public static void main(String[] args) {
+		new DecimalConverter();
+	}
+	public DecimalConverter() {
+		Long laaaaaaaaang = new Long(3405691582L);
+		System.out.println(Long.toHexString(laaaaaaaaang));
+	}
+}
