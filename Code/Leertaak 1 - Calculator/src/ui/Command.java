cd21f50633e779b2c4bfde412bb8faccb767d66e
René Kooi <rene@kooi.me>
2016-02-15 12:29:16 +0100

    ♪ Opgave 25: Add OctalBase

--- Code/Leertaak 1 - Calculator/src/ui/Command.java
+++ Code/Leertaak 1 - Calculator/src/ui/Command.java
@@ -22,7 +22,7 @@ import java.io.*;
 
 /**
  * De main-klasse die leest en schrijft naar de console.
- * 
+ *
  * @author Brugali
  * @author Baljé
  */
@@ -54,6 +54,7 @@ public class Command {
       else if(command.equals("/")) calc.divide();
       else if(command.equals("dec")) calc.setBase(new DecimalBase());
       else if(command.equals("bin")) calc.setBase(new BinaryBase());
+      else if(command.equals("oct")) calc.setBase(new OctalBase());
       else if(command.equals("hex")) calc.setBase(new HexBase());
       else if(command.equals("rat")) calc.setFormat(new RationalFormat());
       else if(command.equals("fixed")) calc.setFormat(new FixedPointFormat());
