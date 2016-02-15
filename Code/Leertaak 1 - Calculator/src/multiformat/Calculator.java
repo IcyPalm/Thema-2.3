b92bcd8384e0503529b5779dbe20aebe1abfab0e
René Kooi <rene@kooi.me>
2016-02-15 16:36:28 +0100

    ⛷ Opgave 29a: Implement operand stack

--- Code/Leertaak 1 - Calculator/src/multiformat/Calculator.java
+++ Code/Leertaak 1 - Calculator/src/multiformat/Calculator.java
@@ -18,61 +18,120 @@
  */
 package multiformat;
 
+import java.util.LinkedList;
+
 /**
  * The multiformat calculator
  */
 public class Calculator {
-  private Rational operand_0 = new Rational();
-  private Rational operand_1 = new Rational();
+	private LinkedList<Rational> operands = new LinkedList<>();
 
-  // The current format of the calculator
-  private Format format = new FixedPointFormat();
-  // The current numberbase of the calculator
-  private Base base = new DecimalBase();
+	// The current format of the calculator
+	private Format format = new FixedPointFormat();
+	// The current numberbase of the calculator
+	private Base base = new DecimalBase();
 
-  public void addOperand(String newOperand) throws FormatException {
-	  operand_1 = operand_0;
-    operand_0 = format.parse(newOperand, base);
-  }
+	/**
+	 * Add an operand to the stack.
+	 *
+	 * @param newOperand Operand value.
+	 */
+	public void addOperand(String newOperand) throws FormatException {
+		Rational operand = this.format.parse(newOperand, this.base);
+		this.operands.push(operand);
+	}
 
-  public void add() {
-    operand_0 = operand_1.plus(operand_0);
-    operand_1 = new Rational();
-  }
-  public void subtract() {
-    operand_0 = operand_1.minus(operand_0);
-    operand_1 = new Rational();
-  }
-  public void multiply() {
-    operand_0 = operand_1.mul(operand_0);
-    operand_1 = new Rational();
-  }
-  public void divide() {
-    operand_0 = operand_1.div(operand_0);
-    operand_1 = new Rational();
-  }
-  public void delete() {
-    operand_0 = operand_1;
-    operand_1 = new Rational();
-  }
+	/**
+	 * Remove the operand at the top of the stack and return it. Default to Zero
+	 * if the stack is empty.
+	 *
+	 * @return Operand.
+	 */
+	private Rational popOperand() {
+		if (this.operands.size() > 0) {
+			return this.operands.pop();
+		}
+		return new Rational();
+	}
 
-  public String firstOperand() {
-    return format.toString(operand_1,base);
-  }
-  public String secondOperand() {
-    return format.toString(operand_0,base);
-  }
+	/**
+	 * Add the two Rationals at the top of the stack.
+	 */
+	public void add() {
+		Rational b = this.popOperand();
+		Rational a = this.popOperand();
+		this.operands.push(a.plus(b));
+	}
+	/**
+	 * Subtract the two Rationals at the top of the stack.
+	 */
+	public void subtract() {
+		Rational b = this.popOperand();
+		Rational a = this.popOperand();
+		this.operands.push(a.minus(b));
+	}
+	/**
+	 * Multiply the two Rationals at the top of the stack.
+	 */
+	public void multiply() {
+		Rational b = this.popOperand();
+		Rational a = this.popOperand();
+		this.operands.push(a.mul(b));
+	}
+	/**
+	 * Divide the two Rationals at the top of the stack.
+	 */
+	public void divide() {
+		Rational b = this.popOperand();
+		Rational a = this.popOperand();
+		this.operands.push(a.div(b));
+	}
+	/**
+	 * Remove the operand at the top of the stack, if it exists.
+	 */
+	public void delete() {
+		if (this.operands.size() > 0) {
+			this.operands.removeFirst();
+		}
+	}
 
-  public void setBase(Base newBase) {
-    base = newBase;
-  }
-  public Base getBase() {
-    return base;
-  }
-  public void setFormat(Format newFormat) {
-    format = newFormat;
-  }
-  public Format getFormat() {
-    return format;
-  }
+	/**
+	 * Return the current first operand, or Zero.
+	 *
+	 * @return First operand value.
+	 */
+	public String firstOperand() {
+		Rational operand = new Rational();
+		if (this.operands.size() >= 2) {
+			// The stack is in reverse order: the second operand is at the top, the
+			// first is just below.
+			operand = this.operands.get(1);
+		}
+		return this.format.toString(operand, this.base);
+	}
+	/**
+	 * Return the current second operand, or Zero.
+	 *
+	 * @return Second operand value.
+	 */
+	public String secondOperand() {
+		Rational operand = new Rational();
+		if (this.operands.size() >= 1) {
+			operand = this.operands.peek();
+		}
+		return this.format.toString(operand, this.base);
+	}
+
+	public void setBase(Base base) {
+		this.base = base;
+	}
+	public Base getBase() {
+		return this.base;
+	}
+	public void setFormat(Format format) {
+		this.format = format;
+	}
+	public Format getFormat() {
+		return this.format;
+	}
 }
