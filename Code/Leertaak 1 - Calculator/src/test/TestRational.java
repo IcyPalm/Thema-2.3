8b474cacaf218c80f6b18341db7e9de9a6623c3f
René Kooi <rene@kooi.me>
2016-02-15 12:09:48 +0100

    📌 fix tests

--- Code/Leertaak 1 - Calculator/src/test/TestRational.java
+++ Code/Leertaak 1 - Calculator/src/test/TestRational.java
@@ -1,5 +1,6 @@
 package test;
 
+import junit.framework.TestCase;
 import static org.junit.Assert.*;
 
 import multiformat.Rational;
@@ -8,64 +9,61 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * JUnit Testcase to test Rational. 
- * Note that this class uses 'annotations' (the @...). This is a Java 1.5 feature. 
+ * JUnit Testcase to test Rational.
+ * Note that this class uses 'annotations' (the @...). This is a Java 1.5 feature.
  * @author gebruiker
  *
  */
-public class TestRational {
+public class TestRational extends TestCase {
 	Rational r;
-	
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		r = new Rational();
 	}
-	
+
 	@Test
 	public void testSimplify() {
 		r.setNumerator(25);
 		r.setDenominator(5);
 		r.simplify();
-		
+
 		assertEquals(5.0, r.getNumerator());
 		assertEquals(1.0, r.getDenominator());
-		
+
 		r.setNumerator(10);
 		r.setDenominator(0.5);
 		r.simplify();
-		
+
 		assertEquals(10.0, r.getNumerator());
-		assertEquals(0.5, r.getDenominator());		
+		assertEquals(0.5, r.getDenominator());
 	}
-	
+
 	@Test
 	public void testCanonical() {
 		r.setNumerator(12.5);
 		r.setDenominator(1.0);
 		r.canonical();
-		
+
 		assertEquals(125.0, r.getNumerator());
 		assertEquals(10.0, r.getDenominator());
 
 		r.setNumerator(12.5);
 		r.setDenominator(0.01);
 		r.canonical();
-		
+
 		assertEquals(125.0, r.getNumerator());
-		assertEquals(0.1, r.getDenominator());	
+		assertEquals(0.1, r.getDenominator());
 	}
-	
+
 	@Test
 	public void testCanonicalAndSimplify() {
 		r.setNumerator(12.5);
 		r.setDenominator(1.0);
 		r.canonical();
 		r.simplify();
-		
+
 		assertEquals(25.0, r.getNumerator());
-		assertEquals(2.0, r.getDenominator());		
+		assertEquals(2.0, r.getDenominator());
 	}
-
-
-
 }
