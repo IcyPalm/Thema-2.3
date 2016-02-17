838af77b1ac578a531478b5134e2ca372de0b695
René Kooi <rene@kooi.me>
2016-02-17 14:56:40 +0100

    📥 Import GetalRij

---
+++ Code/lt2-getalrij/src/getalrij/GetalRij.java
@@ -0,0 +1,56 @@
+package getalrij;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class GetalRij {
+	private int[] getallen;
+
+	public GetalRij(int aantal, int max) {
+		// Belangrijke aanname: aantal < max, anders kunnen de getallen niet uniek zijn.
+		this.getallen = new int[aantal];
+		this.vulArrayMetUniekeWaarden(aantal, max);
+	}
+
+	private void vulArrayMetUniekeWaarden(int aantal, int max) {
+		// Vul een hulplijst met getallen 0, ..., max
+		ArrayList hulpLijst = new ArrayList(max);
+		for (int i = 0; i < max; i++) {
+			hulpLijst.add(i);
+		}
+
+		// Stop 'aantal' random waarden in getallen
+		Random r = new Random();
+		for (int i = 0; i < aantal; i++) {
+			int getal = (Integer) hulpLijst.remove(r.nextInt(hulpLijst.size()));
+			this.getallen[i] = getal;
+		}
+	}
+
+	public boolean zitErinA(int zoekWaarde) {
+		return false;
+	}
+
+	public boolean zitErinB(int zoekWaarde) {
+		return false;
+	}
+
+	public boolean zitErinC(int zoekWaarde) {
+		return false;
+	}
+
+	public boolean zitErinD(int zoekWaarde) {
+		return false;
+	}
+
+	public void sorteer() {
+		Arrays.sort(getallen);
+	}
+
+	public void print() {
+		for (int i = 0; i < getallen.length; i++) {
+			System.out.println(getallen[i]);
+		}
+	}
+}
---
+++ Code/lt2-getalrij/src/getalrij/SnelheidOefening.java
@@ -0,0 +1,15 @@
+package getalrij;
+
+public class SnelheidOefening {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		GetalRij gr = new GetalRij(100000, 200000);
+	}
+
+	// Hulpmethode voor tijdsbepaling
+	private static long tijd() {
+		return System.currentTimeMillis();
+	}
+}
