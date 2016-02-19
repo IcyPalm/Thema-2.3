f08babbca9db467a4fce978312c1be3e0e4b9e9c
René Kooi <rene@kooi.me>
2016-02-19 11:25:13 +0100

    🔨 Fix benchmarks, print total time instead of average

--- Code/lt2-getalrij/src/getalrij/SnelheidOefening.java
+++ Code/lt2-getalrij/src/getalrij/SnelheidOefening.java
@@ -12,19 +12,17 @@ public class SnelheidOefening {
 	public static void main(String[] args) {
 		SnelheidOefening speed = new SnelheidOefening(200, 100000);
 		speed.init();
-		System.out.print("Linear        - zitErinA: ");
-		speed.bench(GetalRij::zitErinA).ifPresent((time) -> {
-			System.out.println(time + "ns");
-		});
-		System.out.print("Linear        - zitErinB: ");
-		speed.bench(GetalRij::zitErinB).ifPresent((time) -> {
-			System.out.println(time + "ns");
-		});
-		System.out.print("Linear Sorted - zitErinC: ");
-		speed.benchSorted(GetalRij::zitErinC).ifPresent((time) -> {
-			System.out.println(time + "ns");
-		});
+
+		printResult("Linear        - zitErinA", speed.bench(GetalRij::zitErinA));
+		printResult("Linear        - zitErinB", speed.bench(GetalRij::zitErinB));
+		printResult("Linear Sorted - zitErinC", speed.benchSorted(GetalRij::zitErinC));
+
 		System.out.println("Done");
+	}
+
+	private static void printResult(String name, long time) {
+		System.out.print(name + ": ");
+		System.out.println(((double) time / 1000 / 1000) + "ms");
 	}
 
 	private long seed = new Random().nextLong();
@@ -54,15 +52,15 @@ public class SnelheidOefening {
 		this.sortedList.sorteer();
 	}
 
-	public OptionalDouble benchSorted(BiPredicate<GetalRij, Integer> contains) {
+	public long benchSorted(BiPredicate<GetalRij, Integer> contains) {
 		return this.bench(this.sortedList, contains);
 	}
 
-	public OptionalDouble bench(BiPredicate<GetalRij, Integer> contains) {
+	public long bench(BiPredicate<GetalRij, Integer> contains) {
 		return this.bench(this.list, contains);
 	}
 
-	public OptionalDouble bench(GetalRij list, BiPredicate<GetalRij, Integer> contains) {
+	public long bench(GetalRij list, BiPredicate<GetalRij, Integer> contains) {
 		LongStream.Builder samples = LongStream.builder();
 		// Reuse seed for fair comparison.
 		Random random = new Random(this.seed);
@@ -72,6 +70,6 @@ public class SnelheidOefening {
 			contains.test(list, getal);
 			samples.add(System.nanoTime() - start);
 		}
-		return samples.build().average();
+		return samples.build().sum();
 	}
 }
