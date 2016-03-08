29643ed42d4805eae6d439628ee67b5332e9b014
Marijn Pool <info@marijnpool.nl>
2016-03-08 10:34:55 +0100

    Fixed some indenting and such

--- Code/lt3-cards/src/cards/Solution.java
+++ Code/lt3-cards/src/cards/Solution.java
@@ -39,73 +39,72 @@ public class Solution extends Stack<Candidate>
 	
 	
 	/**
-	 * Checks whether candidate card of same kind.
-	 * Checks whether by placing candidate the solution sofar still complies with the rules
+	 * Checks whether candidate card of same kind. Checks whether by placing
+	 * candidate the solution sofar still complies with the rules
+	 * 
 	 * @param candidate
-	 * @return boolean indicating whether this candidate can be put in the
-	 * next free position.
+	 * @return boolean indicating whether this candidate can be put in the next
+	 *         free position.
 	 */
-	public boolean fits(Candidate candidate){ 
-		//TODO
-	    return true;
-    }
+	public boolean fits(Candidate candidate) {
+		// TODO
+		return true;
+	}
 
-	public void record(Candidate candidate)
-	{
-		int i=this.size(); // i= index in this stack of next for the next candidate
-		board [row[i]] [column[i]] = candidate; //x=row, y=column
+	public void record(Candidate candidate) {
+		int i = this.size(); // i= index in this stack of next for the next
+								// candidate
+		board[row[i]][column[i]] = candidate; // x=row, y=column
 		this.push(candidate);
-		
+
 	}
 
-	public boolean complete()
-	{
-		return this.size()==8;
+	public boolean complete() {
+		return this.size() == 8;
 	}
 
-	public void show()
-	{
-		System.out.println(this); 
+	public void show() {
+		System.out.println(this);
 	}
 
-	public Candidate eraseRecording()
-	{
-		int i=this.size()-1;           // i= index of the candidate that is removed from this Stack;
-		board[row[i]][column[i]]=null; // remove candidate from board
+	public Candidate eraseRecording() {
+		int i = this.size() - 1; // i= index of the candidate that is removed
+									// from this Stack;
+		board[row[i]][column[i]] = null; // remove candidate from board
 		return this.pop();
-    }
-	
+	}
+
 	// can be used in method isCorrect
-    private char mustBeAdjacentTo(char card)
-    {  
-      if (card=='A') return 'K'; 
-      if (card=='K') return 'Q'; 
-      if (card=='Q') return 'J';
-      return '?'; //error
-    }
-	
+	private char mustBeAdjacentTo(char card) {
+		if (card == 'A')
+			return 'K';
+		if (card == 'K')
+			return 'Q';
+		if (card == 'Q')
+			return 'J';
+		return '?'; // error
+	}
+
 	/**
-	 * Checks whether the rules below are fulfilled
-	 * For the positions that can be checked for solution sofar.
-	 * Rules:
-	 * Elke aas (ace) grenst (horizontaal of verticaal) aan een heer (king).
-	 * Elke heer grenst aan een vrouw (queen).
-	 * Elke vrouw grenst aan een boer (jack).
+	 * Checks whether the rules below are fulfilled For the positions that can
+	 * be checked for solution sofar. Rules: Elke aas (ace) grenst (horizontaal
+	 * of verticaal) aan een heer (king). Elke heer grenst aan een vrouw
+	 * (queen). Elke vrouw grenst aan een boer (jack).
+	 * 
 	 * @return true if all checks are correct.
 	 */
 	// uses methods borderCard and mustBeAdjacent to
 	private boolean isCorrect() {
-         //TODO
-         return true;
-     }     
-            
-	
+		// TODO
+		return true;
+	}
+
 	/**
 	 * @return a representation of the solution on the board
 	 */
-     public String toString(){
-	    //TODO
-	    return "";
-	}    
+	public String toString() {
+		// TODO
+		return "";
+	}
 
 }
