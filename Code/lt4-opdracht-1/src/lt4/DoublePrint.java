481e760354712ba250055acdb16434d38e6cf169
René Kooi <rene@kooi.me>
2016-03-15 11:46:16 +0100

    ⛧ Base code for Opdracht 1, part 3

---
+++ Code/lt4-opdracht-1/src/lt4/DoublePrint.java
@@ -0,0 +1,27 @@
+package lt4;
+
+import java.lang.Thread;
+import java.lang.Runnable;
+
+public class DoublePrint {
+	public static void main(String[] args) {
+		new DoublePrint();
+	}
+
+	public DoublePrint() {
+		for (int i = 1; i <= 4; i++) {
+			new Thread(new Printer(i)).start();
+		}
+	}
+
+	private class Printer implements Runnable {
+		private int number;
+		public Printer(int number) {
+			this.number = number;
+		}
+		public void run() {
+			System.out.print(number);
+			System.out.println(number);
+		}
+	}
+}
