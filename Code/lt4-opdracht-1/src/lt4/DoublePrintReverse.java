934446f2894fc62113325b7e9c0cdd640ed1758b
René Kooi <rene@kooi.me>
2016-03-16 11:38:34 +0100

    🚀 LT4 Opdracht 1: Print in reverse order

--- Code/lt4-opdracht-1/src/lt4/DoublePrintReverse.java
+++ Code/lt4-opdracht-1/src/lt4/DoublePrintReverse.java
@@ -2,6 +2,10 @@ package lt4;
 
 import java.lang.Thread;
 import java.lang.Runnable;
+import java.lang.InterruptedException;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
 
 public class DoublePrintReverse {
 	public static void main(String[] args) {
@@ -14,16 +18,37 @@ public class DoublePrintReverse {
 		}
 	}
 
-	private class PrintTask implements Runnable {
+	private static class PrintTask implements Runnable {
+		private static int currentNumber = 4;
+		private static Lock lock = new ReentrantLock();
+		private static Condition condition = lock.newCondition();
 		private int number;
 
-		public Printer(int number) {
+		public PrintTask(int number) {
 			this.number = number;
 		}
 
-		public void run() {
+		private void print() {
 			System.out.print(number);
 			System.out.println(number);
+		}
+
+		public void run() {
+			lock.lock();
+			try {
+				while (this.number != currentNumber) {
+					condition.await();
+				}
+
+				this.print();
+
+				currentNumber--;
+				condition.signalAll();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				lock.unlock();
+			}
 		}
 	}
 }
